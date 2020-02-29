7161d52b7cfbf87e10226e092fc27783f8cf854c
Philipp Eib <philipp.w.eib@gmail.com>
2020-02-29 16:51:31 +0100

    Add config 'includeEndCharInSelection', fixes #110

--- src/config/finder-config.ts
+++ src/config/finder-config.ts
@@ -2,4 +2,5 @@ export class FinderConfig {
   public pattern = `[ ,-.{_(\"'<\\[\t]`;
   public skipSelection = false;
   public onlyInitialLetter = true;
+  public includeEndCharInSelection = true;
 }
